9a5c9894d4fd8609f869deb50416015f977f6e5b
Andrew Welch <andrew@nystudio107.com>
2019-08-22 09:51:59 -0400

    Add VuePress docs
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Units Documentation',
+    description: 'Documentation for the Units plugin',
+    base: '/docs/units/',
+    dest: '../../../../sites/nystudio107/web/docs/units',
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: [
+            ['/', 'Units plugin for Craft CMS 3.x'],
+        ]
+    }
+};
