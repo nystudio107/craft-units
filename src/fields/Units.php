a4a1115f9c677f7fdf32c8fedabb739c28d2bee1
Andrew Welch <andrew@nystudio107.com>
2025-01-19 20:58:08 -0500

    feat: Add a GraphQL interface for Units fields, closes ([#5](https://github.com/nystudio107/craft-units/issues/5))

--- src/fields/Units.php
+++ src/fields/Units.php
@@ -18,6 +18,7 @@ use craft\base\PreviewableFieldInterface;
 use craft\helpers\Json;
 use craft\i18n\Locale;
 use nystudio107\units\assetbundles\unitsfield\UnitsFieldAsset;
+use nystudio107\units\gql\types\generators\UnitsDataGenerator;
 use nystudio107\units\helpers\ClassHelper;
 use nystudio107\units\models\Settings;
 use nystudio107\units\models\UnitsData;
@@ -25,9 +26,6 @@ use nystudio107\units\Units as UnitsPlugin;
 use nystudio107\units\validators\EmbeddedUnitsDataValidator;
 use PhpUnitsOfMeasure\PhysicalQuantity\Length;
 use yii\base\InvalidConfigException;
-use function is_array;
-use function is_numeric;
-use function is_string;
 
 /**
  * @author    nystudio107
@@ -246,6 +244,20 @@ class Units extends Field implements PreviewableFieldInterface
         }
 
         return '';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getContentGqlType()
+    {
+        $typeArray = UnitsDataGenerator::generateTypes($this);
+
+        return [
+            'name' => $this->handle,
+            'description' => 'Units field',
+            'type' => array_shift($typeArray),
+        ];
     }
 
     /**
